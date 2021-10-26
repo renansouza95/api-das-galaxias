0900896b1c7247a352548d0183ad20805d437c04
Renan Souza <s.renan@outlook.com>
2021-10-26 16:26:06 -0300

    adiciona elementos no arquivo html e css

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
-// Implemente suas HOF aqui
-// const { getScorers, getScorerByName } = require(/fetch/getScorers);
+// const getScorerByName = (championship, player) => {
+// 	const url = `https://football98.p.rapidapi.com/${championship}/scorername/${player}`;
+// 	fetch(url, {
+// 		"method": "GET",
+// 		"headers": {
+// 			"x-rapidapi-host": "football98.p.rapidapi.com",
+// 			"x-rapidapi-key": "eb1bf93565msha6a03a448e1a0d1p1ec5e2jsn65322cd8aa9d"
+// 		}
+// 	})
+// 	.then((response) => response.json())
+// 	.then((data) => data)
+// 	.catch((error) => console.error(error));
+// }
+
+// const getSquadByName = (championship, teamName) => {
+// 	const url = `https://football98.p.rapidapi.com/${championship}/squadname/${teamName}`;
+// 	fetch(url, {
+// 		"method": "GET",
+// 		"headers": {
+// 			"x-rapidapi-host": "football98.p.rapidapi.com",
+// 			"x-rapidapi-key": "b19a964cafmsh62a3c8356170df6p12d7fdjsn0bf25e356dd6"
+// 		}
+// 	})
+// 	.then(response => response.json())
+// 	.then(data => {return data})
+// 	.catch(error => console.error(error));
+// }
 
 const getScorers = (championship) => {
 	const url = `https://football98.p.rapidapi.com/${championship}/scorers`;
@@ -11,37 +36,9 @@ const getScorers = (championship) => {
 		}
 	})
 	.then((response) => response.json())
-	.then((data) => console.log(data))
+	.then((data) => data)
 	.catch((error) => console.error(error));
 }
-
-const getScorerByName = (championship, player) => {
-	const url = `https://football98.p.rapidapi.com/${championship}/scorername/${player}`;
-	fetch(url, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "football98.p.rapidapi.com",
-			"x-rapidapi-key": "eb1bf93565msha6a03a448e1a0d1p1ec5e2jsn65322cd8aa9d"
-		}
-	})
-	.then((response) => response.json())
-	.then((data) => console.log(data))
-	.catch((error) => console.error(error));
-}
-
-const getSquadByName = (championship, teamName) => {
-	const url = `https://football98.p.rapidapi.com/${championship}/squadname/${teamName}`;
-	fetch(url, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "football98.p.rapidapi.com",
-			"x-rapidapi-key": "b19a964cafmsh62a3c8356170df6p12d7fdjsn0bf25e356dd6"
-		}
-	})
-	.then(response => response.json())
-	.then(data => {return data})
-	.catch(error => console.error(error));
-} 
 
 const getSquads = (championship) => {
 	const url = `https://football98.p.rapidapi.com/${championship}/squads`;
@@ -53,9 +50,9 @@ const getSquads = (championship) => {
 		}
 })
 	.then(response => response.json())
-	.then(data => {return data})
+	.then(data => data)
 	.catch(error => console.error(error));
 }
 
 getScorers('premierleague');
-getScorerByName('premierleague', 'kane');
+getSquads('premierleague');
