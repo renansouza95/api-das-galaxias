18e5feb077ec77d1369f9c782a7f41abb2ef7785
FabricioMartins <you@example.com>
2021-10-26 17:56:37 -0300

    [feat] inclusão da funcao fetch api para recuperar as temporadas do campeonato

--- script.js
+++ script.js
@@ -1,111 +1,28 @@
-// const getSquadByName = async (championship, teamName) => {
-// 	const url = `https://football98.p.rapidapi.com/${championship}/squadname/${teamName}`;
-//   fetch(url, {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "football98.p.rapidapi.com",
-// 		"x-rapidapi-key": "b19a964cafmsh62a3c8356170df6p12d7fdjsn0bf25e356dd6"
-// 	}
-// })
-// .then(response => response.json())
-// .then(data =>  data)
-// .catch(err => {
-// 	console.error(err);
-// });
-// } 
+const table = document.querySelector('.seasons');
 
-const getSquads = (championship) => {
-  const url = `https://football98.p.rapidapi.com/${championship}/squads` 
-	fetch(url, {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "football98.p.rapidapi.com",
-		"x-rapidapi-key": "b19a964cafmsh62a3c8356170df6p12d7fdjsn0bf25e356dd6"
-	}
-})
+const getSeasons = async (championship) => {
+  const url = `https://api-football-standings.azharimm.site/leagues/${championship}/seasons` 
+fetch(url)
 .then(response => response.json())
-.then(data => getChampionship(data))
+.then(data => getLeagueSeasons(data))
 .catch(err => {
 	console.error(err);
 });
 } 
-// const getScorerByName = (championship, player) => {
-// 	const url = `https://football98.p.rapidapi.com/${championship}/scorername/${player}`;
-// 	fetch(url, {
-// 		"method": "GET",
-// 		"headers": {
-// 			"x-rapidapi-host": "football98.p.rapidapi.com",
-// 			"x-rapidapi-key": "eb1bf93565msha6a03a448e1a0d1p1ec5e2jsn65322cd8aa9d"
-// 		}
-// 	})
-// 	.then((response) => response.json())
-// 	.then((data) => data)
-// 	.catch((error) => console.error(error));
-// }
 
-// const getSquadByName = (championship, teamName) => {
-// 	const url = `https://football98.p.rapidapi.com/${championship}/squadname/${teamName}`;
-// 	fetch(url, {
-// 		"method": "GET",
-// 		"headers": {
-// 			"x-rapidapi-host": "football98.p.rapidapi.com",
-// 			"x-rapidapi-key": "b19a964cafmsh62a3c8356170df6p12d7fdjsn0bf25e356dd6"
-// 		}
-// 	})
-// 	.then(response => response.json())
-// 	.then(data => {return data})
-// 	.catch(error => console.error(error));
-// }
-
-const getScorers = (championship) => {
-	const url = `https://football98.p.rapidapi.com/${championship}/scorers`;
-	fetch(url, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "football98.p.rapidapi.com",
-			"x-rapidapi-key": "eb1bf93565msha6a03a448e1a0d1p1ec5e2jsn65322cd8aa9d"
-		}
-	})
-	.then((response) => response.json())
-	.then((data) => data)
-	.catch((error) => console.error(error));
+const getLeagueSeasons = (league) => {
+  const seasons = league.data.seasons;
+  const row = document.createElement('tr');
+  const leagueName = document.createElement('th');
+  leagueName.innerHTML = league.data.name;
+  row.appendChild(leagueName)
+  table.appendChild(row)
+  seasons.forEach((season) => {
+    const rowElement = document.createElement('td');
+    rowElement.innerHTML = season.year;
+    row.appendChild(rowElement)
+    table.appendChild(row)
+  });
 }
 
-// const getScorerName = (league, player) => {
-// 	const url = `https://football98.p.rapidapi.com/${league}/scorername/${player}`;
-// 	fetch(url, {
-// 		"method": "GET",
-// 		"headers": {
-// 			"x-rapidapi-host": "football98.p.rapidapi.com",
-// 			"x-rapidapi-key": "eb1bf93565msha6a03a448e1a0d1p1ec5e2jsn65322cd8aa9d"
-// 		}
-// 	})
-// 	.then((response) => response.json())
-// 	.then((data) => data)
-// 	.catch((error) => console.error(error));
-// }
-
-const getChampionship = (league) => {
-  const teams = Array.from(league)
-  teams.forEach((team) => {
-    console.log(team)
-  })
-}
-
-getSquads('ligue1');
-const getSquads = (championship) => {
-	const url = `https://football98.p.rapidapi.com/${championship}/squads`;
-	fetch(url, {
-		"method": "GET",
-		"headers": {
-			"x-rapidapi-host": "football98.p.rapidapi.com",
-			"x-rapidapi-key": "b19a964cafmsh62a3c8356170df6p12d7fdjsn0bf25e356dd6"
-		}
-})
-	.then(response => response.json())
-	.then(data => data)
-	.catch(error => console.error(error));
-}
-
-getScorers('premierleague');
-getSquads('premierleague');
+getSeasons('arg.1')
